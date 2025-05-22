53f783d20eca43c88320963c44c6d9dd6d993b61
benaberg <benjamin.aaberg@outlook.com>
2025-05-22 21:11:42 +0300

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.json:json:20090211")
+    implementation("org.json:json:20250517")
 }
 
 tasks {
     withType<Jar> {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
         manifest {
             attributes["Main-Class"] = "fi.benaberg.sts.STSServiceKt"
         }
+
         configurations["compileClasspath"].forEach { file: File ->
             from(zipTree(file.absoluteFile))
         }
